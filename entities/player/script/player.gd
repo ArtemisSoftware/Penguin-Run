0757543cdc6c99122393c6beec0c6acbe2923678
gustavo <gustavo.isaac@gmail.com>
2025-12-02 15:15:57 +0000

    Player camera

--- entities/player/script/player.gd
+++ entities/player/script/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-const SPEED = 30.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 80.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
