731200c775faad588a37e9d0c2c46442284e50db
gustavo <gustavo.isaac@gmail.com>
2025-12-10 19:37:59 +0000

    State machine for player - idle, jump, walk

--- entities/player/script/player.gd
+++ entities/player/script/player.gd
@@ -2,12 +2,144 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
+enum PlayerState {
+	idle,
+	walk,
+	jump
+}
 
+var status: PlayerState
+
+
+
+func _ready() -> void:
+	go_to_idle_state()
+	pass 
+	
+	
 func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta	
+	
+	
+	match status:
+		PlayerState.idle:
+			idle_state()
+			
+		PlayerState.walk:
+			walk_state()			
+	
+		PlayerState.jump:
+			jump_state()	
+			
+			
+	move_and_slide()			
+	pass	
+	
+	
+#---------------------------
+#Movement 
+#---------------------------		
+	
+func move():
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)		
+	
+	flip(direction)
+	
+#---------------------------
+#Movement - states
+#---------------------------	
+
+func go_to_idle_state():
+	status = PlayerState.idle
+	animated_sprite_2d.play("idle")
+	pass
+
+func idle_state():
+	move()
+	
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		go_to_jump_state()
+		return	
+	pass
+
+
+
+func go_to_walk_state():
+	status = PlayerState.walk
+	animated_sprite_2d.play("walk")
+	pass
+	
+func walk_state():
+	move()
+	
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		go_to_jump_state()
+		return			
+	pass
+	
+	
+	
+func go_to_jump_state():
+	status = PlayerState.jump
+	animated_sprite_2d.play("jump")
+	velocity.y = JUMP_VELOCITY
+	pass
+		
+func jump_state():
+	move()
+	
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()	
+		return
+	pass	
+
+
+func flip(direction: float) -> void:
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+		#animated_sprite_2d.play("walk")
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true	
+		#animated_sprite_2d.play("walk")
+	else: 
+		print("")
+		#animated_sprite_2d.play("idle")
+		
+
+
+
+
+
+
+
+
+
+
+func _physics_process_v1(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -30,18 +162,4 @@ func _physics_process(delta: float) -> void:
 	else: 	animated_sprite_2d.play("jump")
 
 	move_and_slide()
-	
-	
-#---------------------------
-#Movement
-#---------------------------	
-
-func flip(direction: float) -> void:
-	if direction > 0:
-		animated_sprite_2d.flip_h = false
-		animated_sprite_2d.play("walk")
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true	
-		animated_sprite_2d.play("walk")
-	else: 
-		animated_sprite_2d.play("idle")
+			
