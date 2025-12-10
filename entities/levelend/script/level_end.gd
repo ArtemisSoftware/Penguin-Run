f18b9ca0b971ec9d64f3e570a26d7232833c2a15
gustavo <gustavo.isaac@gmail.com>
2025-12-10 19:09:52 +0000

    Changing scene - switch scenes

--- entities/levelend/script/level_end.gd
+++ entities/levelend/script/level_end.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var next_level = ""
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,9 +9,13 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
+	call_deferred("load_next_scene")
 	pass # Replace with function body.
+
+func load_next_scene():
+	get_tree().change_scene_to_file("res://levels/" + next_level +"/" + next_level + ".tscn")
